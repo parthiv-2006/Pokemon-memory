49a00d4a93ce4b27dbfd661cf27b13c3f33aa566
parthiv-2006 <parthiv.paul5545@gmail.com>
2025-08-08 16:46:57 -0400

    style: Implement style for Card Grid component

--- src/components/CardGrid.jsx
+++ src/components/CardGrid.jsx
@@ -1,8 +1,10 @@
 import Card from "./Card.jsx";
+import style from "../styles/CardGrid.module.css";
+
 
 function CardGrid({ cards, handleCardClick }) {
   return (
-    <div>
+    <div className={style["card-grid"]}>
       {cards.map((card) => {
         return <Card key={card.id} cardContent={card} handleCardClick={handleCardClick}/>;
       })}
